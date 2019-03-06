08cd02b40bbbbf22bbbdf29fad3fbac4f44ac75e
Vlad Calderon <melezio@gmail.com>
2019-03-06 13:56:30 -0600

    Official Version 15.403 Bug@Saving and Plotting Data

--- Save_and_Plot.m
+++ Save_and_Plot.m
@@ -1,4 +1,7 @@
+Update_Directory;
 OddsMatrix=getoddmatrix(isSIGNALS,notSIGNALS);
+Experiment=Experiment(Experiment~='\');     % NAMES PATCH
+
 %% Plot Final Data
 % GET Condition's Rasters & Whole RASTER 
 [~,Raster_Condition,RASTER_CONCAT]=SortNeuronsCondition(RASTER);
@@ -7,8 +10,8 @@ TotalActiveNeurons=find(sum(RASTER_CONCAT,2)>0);
 QoE=round(100*length(TotalActiveNeurons)/length(XY),2);
 fprintf('Actual \nActive \nNeurons: %d %%\n',round(QoE));
 % SEE RESULTS ################################################
-Plot_Raster_V(RASTER_CONCAT,fs);                           % Clean Whole Raster
-set(gcf,'Name',['ID: ',Experiment(2:end)],'NumberTitle','off')
+Plot_Raster_Ensembles(RASTER_CONCAT,fs);                           % Clean Whole Raster
+set(gcf,'Name',['ID: ',Experiment],'NumberTitle','off')
 Label_Condition_Raster(Names_Conditions,Raster_Condition,fs);   % Labels    
 %% Update Data: VisualInspector,Oddsmatrix, Raster_Condition
 checkname=1; oksave=false;
