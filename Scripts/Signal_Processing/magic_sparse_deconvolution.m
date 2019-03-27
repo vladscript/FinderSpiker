edcf72b8bce47def391b142c96fce29a7e14a7d6
Vlad Calderon <melezio@gmail.com>
2019-03-26 20:09:06 -0600

    Official Version 15.422 Update Response Size

--- Scripts/Signal_Processing/magic_sparse_deconvolution.m
+++ Scripts/Signal_Processing/magic_sparse_deconvolution.m
@@ -11,6 +11,12 @@
 function [d,x_est,MSEE]=magic_sparse_deconvolution(x,r,lambda)
         
 L=length(r);
+Np=numel(x);
+% To avoid probles with the size of the response
+if L>Np
+    r=r(1:Np);
+    L=Np;    
+end
 % Build Response Matrix
 Cols = [r';zeros(abs(length(x)-L),1)];
 Rows = [r(1),zeros(1,length(x)-1)];
