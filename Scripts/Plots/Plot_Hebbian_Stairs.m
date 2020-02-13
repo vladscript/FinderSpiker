ac0b09dcebb3ca4650ffbb5711c790f255a5684a
Vlad Calderon <melezio@gmail.com>
2020-02-13 11:03:34 -0600

    Hebbian Stairs Plots Updated

--- Scripts/Plots/Plot_Hebbian_Stairs.m
+++ Scripts/Plots/Plot_Hebbian_Stairs.m
@@ -9,8 +9,10 @@ if exist('Features_Condition','var')
     EnsembleSequence=[];
     N=numel(Features_Condition.HebbianSeq);
     Naux=1;
+    LimitX=0;
     for n=1:N
-        figure('NumberTitle','off','Name',['Neuronal Ensembles Sequence ',Names_Conditions{n}]); hold on;
+        figure('NumberTitle','off','Name',['Neuronal Ensembles Sequence ',Names_Conditions{n}],...
+            'Position',[730 470-40*(n-1) 560 151]); hold on;
         % LINE ------------------------------------------------------------
         plot(Naux:Naux+numel(Features_Condition.HebbianSeq{n})-1,...
             Features_Condition.HebbianSeq{n},...
@@ -29,14 +31,21 @@ if exist('Features_Condition','var')
         EnsembleSequence=[EnsembleSequence;Features_Condition.HebbianSeq{n}];
         hold off;
         ylabel('Neuronal Ensemble')
-        xlabel('Neuronal Ensemble Instance')
+        xlabel('Neuronal Ensemble Realization')
         CurrAxis{n}=gca;
         Ydiff(n)=diff(CurrAxis{n}.YLim);
+        CurrAxis{n}.XLim(1)=LimitX;
+        LimitX=CurrAxis{n}.XLim(end);
     end
     [MaxAxisY,INdexY]=max(Ydiff);
     for n=1:N
         CurrAxis{n}.YLim(2)=CurrAxis{n}.YLim(2)+MaxAxisY-diff(CurrAxis{n}.YLim);
     end
+    fprintf('####################################################\n')
+    fprintf('#                                                  #\n')
+    fprintf('#  Check Y- and X- axis match for more experiments #\n')
+    fprintf('#                                                  #\n')
+    fprintf('####################################################\n\n')
 else
     fprintf('Missing Clustering Analysis, please execute:\n')
     fprintf('>>R_CONDITIONi_Analysis=get_bays_ensembles(R_CONDTIONi);\n')
